0990fce9b2546fb8ded5744619d2abbf54fa4391
gunturtegar <tegar8797@gmail.com>
2018-11-19 01:04:31 +0700

    retrofit
    tugas modul 10

---
+++ retrofit_/retrofit_/app/src/main/java/asus/com/retrofit/ClickListener.java
@@ -0,0 +1,8 @@
+package asus.com.retrofit;
+
+import android.view.View;
+
+public interface ClickListener {
+    void onClick(View view, int position);
+    void onLongClick(View view, int position);
+}
